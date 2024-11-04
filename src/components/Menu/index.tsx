a70ff5194ec465c86a4d356bdfec61c8f62ee307
h4nsln3r <68617162+h4nsln3r@users.noreply.github.com>
2024-11-04 21:42:04 +0100

    Start on a new Menu
    New menu with animation
    /Todo check luger och terra!

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,24 +1,48 @@
 import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 import { PATHS } from "../../Routing";
+import MenuIcon from '@mui/icons-material/Menu';
+import MenuOpenIcon from '@mui/icons-material/MenuOpen';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+
 import "./menu.scss";
+import { useState } from "react";
 
 const Menu = () => {
+  const [ isOpen, setIsOpen ] = useState<boolean>(false)
+  
   return (
-    <nav className="menu">
-      <ul>
-        <li>
-          <Link to={PATHS.START}>Totes</Link>
-        </li>
-        <li>
-          <Link to={PATHS.WATCH}>Watch</Link>
-        </li>
-        <li>
-          <Link to={PATHS.MUSIC}>Music</Link>
-        </li>
-        <li>
-          <Link to={PATHS.CONTACT}>Contact</Link>
-        </li>
-      </ul>
+    <nav className="menu">   
+      <Link className="menu__logo" to={PATHS.START}>Totes</Link>
+       
+      <div className="menu__icon" onClick={() => setIsOpen(!isOpen)}>
+        {!isOpen ? <MenuIcon /> : <ArrowForwardIosIcon />}
+      </div>
+
+      <motion.div
+        animate={{ width: isOpen ? 300 : 0 }}
+        initial={{ width: 0 }}
+        transition={{ duration: 0.5 }}
+        style={{
+          background: "lightblue",
+          height: 50,
+          overflow: "hidden",
+          originX: 1, // 0 Startar expansionen från vänster 1 FRÅN HÖGER
+        }}
+      >      
+          <ul>
+            <li>
+              <Link to={PATHS.WATCH}>Watch</Link>
+            </li>
+            <li>
+              <Link to={PATHS.MUSIC}>Music</Link>
+            </li>
+            <li>
+              <Link to={PATHS.CONTACT}>Contact</Link>
+            </li>
+          </ul>
+          <div><MenuOpenIcon /></div>
+        </motion.div>
     </nav>
   );
 };

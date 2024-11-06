56bfe26cfd8025e5d5dead334684499e03472ba1
h4nsln3r <h4nsln3r@gmail.com>
2024-11-06 17:17:27 +0100

    Totes menu update

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,20 +1,22 @@
 import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
 import { PATHS } from "../../Routing";
-import MenuIcon from '@mui/icons-material/Menu';
-import MenuOpenIcon from '@mui/icons-material/MenuOpen';
-import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import MenuIcon from "@mui/icons-material/Menu";
+import MenuOpenIcon from "@mui/icons-material/MenuOpen";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
 import "./menu.scss";
 import { useState } from "react";
 
 const Menu = () => {
-  const [ isOpen, setIsOpen ] = useState<boolean>(false)
-  
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
   return (
-    <nav className="menu">   
-      <Link className="menu__logo" to={PATHS.START}>Totes</Link>
-       
+    <nav className="menu">
+      <Link className="menu__logo" to={PATHS.START}>
+        Totes
+      </Link>
+
       <div className="menu__icon" onClick={() => setIsOpen(!isOpen)}>
         {!isOpen ? <MenuIcon /> : <ArrowForwardIosIcon />}
       </div>
@@ -25,24 +27,22 @@ const Menu = () => {
         transition={{ duration: 0.5 }}
         style={{
           background: "lightblue",
-          height: 50,
           overflow: "hidden",
           originX: 1, // 0 Startar expansionen från vänster 1 FRÅN HÖGER
         }}
-      >      
-          <ul>
-            <li>
-              <Link to={PATHS.WATCH}>Watch</Link>
-            </li>
-            <li>
-              <Link to={PATHS.MUSIC}>Music</Link>
-            </li>
-            <li>
-              <Link to={PATHS.CONTACT}>Contact</Link>
-            </li>
-          </ul>
-          <div><MenuOpenIcon /></div>
-        </motion.div>
+      >
+        <ul>
+          <li>
+            <Link to={PATHS.WATCH}>Watch</Link>
+          </li>
+          <li>
+            <Link to={PATHS.MUSIC}>Music</Link>
+          </li>
+          <li>
+            <Link to={PATHS.CONTACT}>Contact</Link>
+          </li>
+        </ul>
+      </motion.div>
     </nav>
   );
 };

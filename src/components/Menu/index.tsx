b9d60e1e5c294b4d54413ae6d1cd5b1684fb6a5e
h4nsln3r <h4nsln3r@gmail.com>
2024-11-08 21:19:56 +0100

    Added active state to menu + some style

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,46 +1,72 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { motion } from "framer-motion";
 import { PATHS } from "../../Routing";
 import MenuIcon from "@mui/icons-material/Menu";
-// import MenuOpenIcon from "@mui/icons-material/MenuOpen";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
 import "./menu.scss";
 import { useState } from "react";
+import classNames from "classnames";
 
 const Menu = () => {
-  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [isOpen, setIsOpen] = useState(false);
+  const location = useLocation(); // Hämta den aktuella sökvägen
 
   return (
     <nav className="menu">
       <Link className="menu__logo" to={PATHS.START}>
         Totes
       </Link>
-      <div className="menu__icon" onClick={() => setIsOpen(!isOpen)}>
-        {!isOpen ? <MenuIcon /> : <ArrowForwardIosIcon />}
+
+      <div
+        className={classNames("menu__icon", {
+          "menu__icon--close": isOpen,
+        })}
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        {!isOpen ? (
+          <MenuIcon fontSize="large" />
+        ) : (
+          <ArrowForwardIosIcon fontSize="large" />
+        )}
       </div>
-      {/* //Nashi */}
+
       <motion.div
+        className="menu__animation"
         animate={{ width: isOpen ? 300 : 0 }}
         initial={{ width: 0 }}
         transition={{ duration: 0.6 }}
-        className=""
-        style={{
-          // background: "lightblue",
-          borderColor: "white",
-          overflow: "hidden",
-          originX: 1, // 0 Startar expansionen från vänster 1 FRÅN HÖGER
-        }}
       >
         <ul>
           <li>
-            <Link to={PATHS.WATCH}>Watch</Link>
+            <Link
+              className={classNames("", {
+                active: location.pathname === PATHS.WATCH,
+              })}
+              to={PATHS.WATCH}
+            >
+              Watch
+            </Link>
           </li>
           <li>
-            <Link to={PATHS.MUSIC}>Music</Link>
+            <Link
+              className={classNames("", {
+                active: location.pathname === PATHS.MUSIC,
+              })}
+              to={PATHS.MUSIC}
+            >
+              Music
+            </Link>
           </li>
           <li>
-            <Link to={PATHS.CONTACT}>Contact</Link>
+            <Link
+              className={classNames("", {
+                active: location.pathname === PATHS.CONTACT,
+              })}
+              to={PATHS.CONTACT}
+            >
+              Contact
+            </Link>
           </li>
         </ul>
       </motion.div>

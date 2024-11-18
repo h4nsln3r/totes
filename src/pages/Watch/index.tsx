627dc7a5db4c3128449780e821f73f9d7a0d72cc
h4nsln3r <h4nsln3r@gmail.com>
2024-11-18 18:52:58 +0100

    Accordions func
    Worked on navigation in watch

--- src/pages/Watch/index.tsx
+++ src/pages/Watch/index.tsx
@@ -10,14 +10,28 @@ import { FaClock, FaFacebook } from "react-icons/fa";
 import { SiGooglemaps } from "react-icons/si";
 
 const Watch: React.FC = () => {
-  // Sätter showUpcoming till true från början
   const [showUpcoming, setShowUpcoming] = useState<boolean>(true);
-  const [showPast, setShowPast] = useState<boolean>(false);
-
-  const [expandedItem, setExpandedItem] = useState<number | null>(null);
-
+  const [expandedCommingItem, setExpandedCommingItem] = useState<number | null>(
+    null
+  );
   const handleClick = (index: number) => {
-    setExpandedItem(expandedItem === index ? null : index);
+    setExpandedCommingItem(expandedCommingItem === index ? null : index);
+  };
+
+  function resetExpandedCommingItem() {
+    if (expandedCommingItem !== null) {
+      setExpandedCommingItem(null);
+    } else {
+      setShowUpcoming(!showUpcoming);
+    }
+  }
+
+  const [showPast, setShowPast] = useState<boolean>(false);
+  const [expandedPreviousItem, setExpandedPreviousItem] = useState<
+    number | null
+  >(null);
+  const handleClick2 = (index: number) => {
+    setExpandedPreviousItem(expandedPreviousItem === index ? null : index);
   };
 
   const content = [
@@ -48,17 +62,18 @@ const Watch: React.FC = () => {
   return (
     <div className="container container__center">
       <div className="watchlist">
-        {!expandedItem ? (
-          <div
-            className="watchlist__accordion"
-            onClick={() => setShowUpcoming(!showUpcoming)}
-            style={{ cursor: "pointer" }}
-          >
+        <div
+          className="watchlist__accordion"
+          onClick={() => resetExpandedCommingItem()}
+          style={{ cursor: "pointer" }}
+        >
+          {expandedCommingItem === null ? (
             <h4>Kommande spelningar {showUpcoming ? "▲" : "▼"}</h4>
-          </div>
-        ) : (
-          <></>
-        )}
+          ) : (
+            <div>X</div>
+          )}
+        </div>
+
         <AnimateHeight height={showUpcoming ? "auto" : 0} duration={500}>
           {/* <ul>
             {upcomingGigs.map((gig) => (
@@ -73,8 +88,10 @@ const Watch: React.FC = () => {
                 <div
                   key={index}
                   className={classNames("grid-item", {
-                    expanded: expandedItem === index,
-                    hide: expandedItem !== null && expandedItem !== index,
+                    expanded: expandedCommingItem === index,
+                    hide:
+                      expandedCommingItem !== null &&
+                      expandedCommingItem !== index,
                   })}
                   onClick={() => handleClick(index)}
                   style={{
@@ -83,12 +100,14 @@ const Watch: React.FC = () => {
                     backgroundPosition: "center",
                   }}
                 >
-                  {expandedItem === index && (
+                  {expandedCommingItem === index && (
                     <>
                       <div
                         className={classNames("grid-item--expanded", {
-                          expanded: expandedItem === index,
-                          hide: expandedItem !== null && expandedItem !== index,
+                          expanded: expandedCommingItem === index,
+                          hide:
+                            expandedCommingItem !== null &&
+                            expandedCommingItem !== index,
                         })}
                         style={{
                           backgroundImage: `url(${item.mainImage})`,
@@ -124,16 +143,17 @@ const Watch: React.FC = () => {
             ))}
           </div>
         </AnimateHeight>
-
-        <div
-          className="watchlist__accordion"
-          onClick={() => setShowPast(!showPast)}
-          style={{ cursor: "pointer" }}
-        >
-          <h4>Tidigare spelningar {showPast ? "▲" : "▼"}</h4>
-        </div>
-        <AnimateHeight height={showPast ? "auto" : 0} duration={500}>
-          {/* <ul>
+        {expandedCommingItem === null ? (
+          <>
+            <div
+              className="watchlist__accordion"
+              onClick={() => setShowPast(!showPast)}
+              style={{ cursor: "pointer" }}
+            >
+              <h4>Tidigare spelningar {showPast ? "▲" : "▼"}</h4>
+            </div>
+            <AnimateHeight height={showPast ? "auto" : 0} duration={500}>
+              {/* <ul>
             {pastGigs.map((gig) => (
               <li key={gig.id}>
                 <strong>{gig.title}</strong> - {gig.date} - {gig.location}
@@ -141,63 +161,71 @@ const Watch: React.FC = () => {
             ))}
           </ul> */}
 
-          <div className="grid">
-            {content2.map((item, index) => (
-              <>
-                <div
-                  key={index}
-                  className={classNames("grid-item", {
-                    expanded: expandedItem === index,
-                    hide: expandedItem !== null && expandedItem !== index,
-                  })}
-                  onClick={() => handleClick(index)}
-                  style={{
-                    backgroundImage: `url(${item.backgroundImage})`,
-                    backgroundSize: "cover",
-                    backgroundPosition: "center",
-                  }}
-                >
-                  {expandedItem === index && (
-                    <>
-                      <div
-                        className={classNames("grid-item--expanded", {
-                          expanded: expandedItem === index,
-                          hide: expandedItem !== null && expandedItem !== index,
-                        })}
-                        style={{
-                          backgroundImage: `url(${item.mainImage})`,
-                          backgroundSize: "cover",
-                          backgroundPosition: "center",
-                        }}
-                      ></div>
-                      <Footer
-                        children={
-                          <div className="flex--row">
-                            <div>
-                              <Link to="https://www.facebook.com/events/575031255206352/">
-                                <FaFacebook />
-                              </Link>
-                            </div>
-                            <div>
-                              <Link to="https://www.google.com/maps/dir/?api=1&destination=55.59404%2C13.02338&fbclid=IwY2xjawGhm1xleHRuA2FlbQIxMAABHQRRtiayl5Kz0eOjaYJ3CSJj0aQ58GLjMDIJgfN8Y6uk6zm860u7YOuigg_aem_yWGOq57B-2uLo4dxqZ3a3g">
-                                <SiGooglemaps />
-                              </Link>
-                            </div>
-                            <div>
-                              <FaClock />
-                            </div>
-                          </div>
-                        }
-                      />
-                    </>
-                  )}
-                  {/* <h2>{item.title}</h2>
+              <div className="grid">
+                {content2.map((item, index) => (
+                  <>
+                    <div
+                      key={index}
+                      className={classNames("grid-item", {
+                        expanded: expandedPreviousItem === index,
+                        hide:
+                          expandedPreviousItem !== null &&
+                          expandedPreviousItem !== index,
+                      })}
+                      onClick={() => handleClick2(index)}
+                      style={{
+                        backgroundImage: `url(${item.backgroundImage})`,
+                        backgroundSize: "cover",
+                        backgroundPosition: "center",
+                      }}
+                    >
+                      {expandedPreviousItem === index && (
+                        <>
+                          <div
+                            className={classNames("grid-item--expanded", {
+                              expanded: expandedPreviousItem === index,
+                              hide:
+                                expandedPreviousItem !== null &&
+                                expandedPreviousItem !== index,
+                            })}
+                            style={{
+                              backgroundImage: `url(${item.mainImage})`,
+                              backgroundSize: "cover",
+                              backgroundPosition: "center",
+                            }}
+                          ></div>
+                          <Footer
+                            children={
+                              <div className="flex--row">
+                                <div>
+                                  <Link to="https://www.facebook.com/events/575031255206352/">
+                                    <FaFacebook />
+                                  </Link>
+                                </div>
+                                <div>
+                                  <Link to="https://www.google.com/maps/dir/?api=1&destination=55.59404%2C13.02338&fbclid=IwY2xjawGhm1xleHRuA2FlbQIxMAABHQRRtiayl5Kz0eOjaYJ3CSJj0aQ58GLjMDIJgfN8Y6uk6zm860u7YOuigg_aem_yWGOq57B-2uLo4dxqZ3a3g">
+                                    <SiGooglemaps />
+                                  </Link>
+                                </div>
+                                <div>
+                                  <FaClock />
+                                </div>
+                              </div>
+                            }
+                          />
+                        </>
+                      )}
+                      {/* <h2>{item.title}</h2>
                   <p>{item.description}</p> */}
-                </div>
-              </>
-            ))}
-          </div>
-        </AnimateHeight>
+                    </div>
+                  </>
+                ))}
+              </div>
+            </AnimateHeight>
+          </>
+        ) : (
+          <></>
+        )}
       </div>
     </div>
   );

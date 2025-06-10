d4abf9b4355bf62da5b45d45787008041827ebd8
h4nsln3r <h4nsln3r@gmail.com>
2025-06-10 17:35:45 +0200

    More structure

--- src/sections/Start/index.tsx
+++ src/sections/Start/index.tsx
@@ -1,5 +1,7 @@
 import SectionWrapper from "../../components/SectionWrapper";
 
+import "./start.scss";
+
 import totesLogoTop from "../../assets/logo/totes_line_1.png";
 import totesLogoBottom from "../../assets/logo/totes_line_2.png";
 import totespic from "../../assets/images/totespic.png";
@@ -11,25 +13,23 @@ interface Props {
 
 const Start: React.FC<Props> = ({ isMobile }) => {
   return (
-    <SectionWrapper id="start">
-      <div className="hero">
-        <div className="logo-top">
-          <img src={totesLogoTop} alt="Totes Logo" className="logo-img" />
-        </div>
+    <SectionWrapper id="start" className="hero">
+      <div className="logo-top">
+        <img src={totesLogoTop} alt="Totes Logo" className="logo-img" />
+      </div>
 
-        <div className="hero-bottom-row">
-          {isMobile ? (
-            <img src={totespic} alt="Band" className="band-img" />
-          ) : (
-            <img
-              src={totesabout}
-              alt="Band"
-              className="band-img band-img--mobile"
-            />
-          )}
-          <div className="logo-bottom">
-            <img src={totesLogoBottom} alt="Totes Logo" className="logo-img" />
-          </div>
+      <div className="hero-bottom-row">
+        {isMobile ? (
+          <img src={totespic} alt="Band" className="band-img" />
+        ) : (
+          <img
+            src={totesabout}
+            alt="Band"
+            className="band-img band-img--mobile"
+          />
+        )}
+        <div className="logo-bottom">
+          <img src={totesLogoBottom} alt="Totes Logo" className="logo-img" />
         </div>
       </div>
     </SectionWrapper>

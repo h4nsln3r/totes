c11ed0f775dd1a34fc0e5a9850f2442c25cd3a07
h4nsln3r <h4nsln3r@gmail.com>
2025-08-26 09:16:23 +0200

    Added gabbes text

--- src/sections/Info/index.tsx
+++ src/sections/Info/index.tsx
@@ -6,6 +6,15 @@ const InfoSection: React.FC<Props> = () => {
   return (
     <SectionWrapper id="info">
       <h2 className="section__title">Om</h2>
+      <p>
+        Med starka melodier och svängiga rytmer rör sig Malmö-trion Totes
+        lekfullt men självsäkert mellan rock, funk, blues och jazz. Bandet
+        består av Svante Berg, Hannes Linnér och Gabriel Fager Ferrari, som
+        sedan 2023 har spelat sina medryckande poplåtar med ett gemensamt mål:
+        att sprida glädje och uppmuntra till dans. Med stilsäker gitarr,
+        mullrande bas och trummor med precis rätt tempo, bjuder Totes även på
+        vacker stämsång och en skopa sexig saxofon
+      </p>
     </SectionWrapper>
   );
 };

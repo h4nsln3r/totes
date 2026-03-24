652ac9394e731541bba470dcde25f5458dd4ae54
h4nsln3r <68617162+h4nsln3r@users.noreply.github.com>
2026-03-24 18:03:35 +0100

    Removed Music title

--- src/sections/Music/index.tsx
+++ src/sections/Music/index.tsx
@@ -1,25 +1,25 @@
-import { useTranslation } from "react-i18next";
-import MuxPlayer from "@mux/mux-player-react";
+import { useTranslation } from 'react-i18next';
+import MuxPlayer from '@mux/mux-player-react';
 
-import SectionWrapper from "..";
-import "./music.scss";
+import SectionWrapper from '..';
+import './music.scss';
 
-const MUSIC_PLAYBACK_ID = "THOaoITPqwRE01eluqYK5O02B79601myxS02JQA5S00CqNxo";
+const MUSIC_PLAYBACK_ID = 'THOaoITPqwRE01eluqYK5O02B79601myxS02JQA5S00CqNxo';
 const MUSIC_POSTER_URL = `https://image.mux.com/${MUSIC_PLAYBACK_ID}/thumbnail.jpg?time=0`;
 
 const Music: React.FC = () => {
   const { t } = useTranslation();
-  const title = t("music.title");
+  // const title = t('music.title');
 
   return (
     <SectionWrapper sectionName="music">
-      <div className="music__title-column">
+      {/* <div className="music__title-column">
         <h2 className="section__title music__title">
           {title.split("").map((char, i) => (
             <span key={i} className="music__title-letter">{char}</span>
           ))}
         </h2>
-      </div>
+      </div> */}
 
       <div className="video-viewport">
         <MuxPlayer

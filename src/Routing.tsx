bcbaf050abb10379e95751485a8ea2dad0acc325
h4nsln3r <68617162+h4nsln3r@users.noreply.github.com>
2026-03-24 17:50:55 +0100

    TOtes

--- src/Routing.tsx
+++ src/Routing.tsx
@@ -1,30 +1,26 @@
-import { useState } from "react";
-import Helmet from "react-helmet";
+import { useState } from 'react';
+import Helmet from 'react-helmet';
 
-import { useMediaQuery } from "./hooks/useMediaQuery";
+import { useMediaQuery } from './hooks/useMediaQuery';
 
-import Menu from "./components/Menu";
-import Hero from "./sections/Hero";
-import Music from "./sections/Music";
-import Live from "./sections/Live";
+import Menu from './components/Menu';
+import Hero from './sections/Hero';
+import Music from './sections/Music';
+import Live from './sections/Live';
 // import Merch from "./sections/Merch";
-import About from "./sections/About";
+import About from './sections/About';
 
 const Routing = () => {
-  const isMobile = useMediaQuery("(max-width: 767px)");
+  const isMobile = useMediaQuery('(max-width: 767px)');
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
     <>
       <Helmet>
-        <title>OTES</title>
+        <title>TOTES</title>
       </Helmet>
 
-      <Menu
-        isOpen={isMenuOpen}
-        setIsOpen={setIsMenuOpen}
-        isMobile={isMobile}
-      />
+      <Menu isOpen={isMenuOpen} setIsOpen={setIsMenuOpen} isMobile={isMobile} />
 
       <main>
         <Hero isMobile={isMobile} isMenuOpen={isMenuOpen} />

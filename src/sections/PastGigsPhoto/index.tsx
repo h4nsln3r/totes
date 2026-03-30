10c05550f3f4a9f4a0ce86a54313fba7c06293ea
h4nsln3r <h4nsln3r@gmail.com>
2026-03-30 20:42:15 +0200

    One last section?

--- src/sections/PastGigsPhoto/index.tsx
+++ src/sections/PastGigsPhoto/index.tsx
@@ -1,17 +1,16 @@
-import { useTranslation } from 'react-i18next';
-
 import SectionWrapper from '..';
 import eventPic from '../../assets/gigs/eventpic.jpg';
+import totesOrig from '../../assets/images/totesorig.jpg';
 import './past-gigs-photo.scss';
 
 const PastGigsPhoto = () => {
-  const { t } = useTranslation();
-
   return (
     <SectionWrapper sectionName="past-gigs" className="past-gigs-photo">
       <div className="past-gigs-photo__frame">
-        <h2 className="past-gigs-photo__title">{t('pastGigs.title')}</h2>
-        <img className="past-gigs-photo__image" src={eventPic} alt="" draggable={false} />
+        <picture>
+          <source media="(min-width: 768px)" srcSet={totesOrig} />
+          <img className="past-gigs-photo__image" src={eventPic} alt="" draggable={false} />
+        </picture>
       </div>
     </SectionWrapper>
   );
